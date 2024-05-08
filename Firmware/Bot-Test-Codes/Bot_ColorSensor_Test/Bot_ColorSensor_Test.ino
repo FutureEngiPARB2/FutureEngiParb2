bf817cadd56cc5e2a1b56451bf1c3d5d95614f95
robotixdevteam <96867511+robotixdevteam@users.noreply.github.com>
2024-05-08 13:53:28 +0530

    Update Bot_ColorSensor_Test.ino

--- Firmware/Bot-Test-Codes/Bot_ColorSensor_Test/Bot_ColorSensor_Test.ino
+++ Firmware/Bot-Test-Codes/Bot_ColorSensor_Test/Bot_ColorSensor_Test.ino
@@ -6,25 +6,23 @@
 
 // Library
 #include <Wire.h>
-#include "Adafruit_TCS34725.h"
+#include <Adafruit_TCS34725.h>
 
 ///////////////////color sensor///////////////////
 
 #define PCA9548A_ADDRESS 0x70  //mux address 0x70
 #define TCS3414CS_ADDRESS 0x29 //ColorSensor address 0x29
 
-Adafruit_TCS34725 tcs1 = Adafruit_TCS34725(TCS34725_INTEGRATIONTIME_154MS, TCS34725_GAIN_1X); // Initializing ColorSensor
-Adafruit_TCS34725 tcs2 = Adafruit_TCS34725(TCS34725_INTEGRATIONTIME_154MS, TCS34725_GAIN_1X);
+// Initializing ColorSensor
+Adafruit_TCS34725 tcs1 = Adafruit_TCS34725(TCS34725_INTEGRATIONTIME_2_4MS, TCS34725_GAIN_4X); // Front Color Sensor
+Adafruit_TCS34725 tcs2 = Adafruit_TCS34725(TCS34725_INTEGRATIONTIME_2_4MS, TCS34725_GAIN_4X); // Back Color Sensor
 
 void setup() {
   Serial.begin(115200);
   analogReadResolution(12);       // Set ADC resolution to 12 bits (0-4095)
   analogSetAttenuation(ADC_0db);  // Set attenuation to 0dB (for full-scale voltage range)
 }
-void loop() {
-  frontcoloursensor();
-  backcoloursensor();
-}
+
 // Assigning the channel for I2C Communication
 void TCA9548A(uint8_t bus) {
   Wire.beginTransmission(PCA9548A_ADDRESS);
@@ -33,65 +31,32 @@ void TCA9548A(uint8_t bus) {
   // delay(100);
 }
 
+
+
 //ColorSensor Function
-void frontcoloursensor() {
+void front_colour_sensor() {
   TCA9548A(0);
-  //delay(100);
   uint16_t r, g, b, c;
-  // Read the color data from the TCS34725 sensor
   tcs1.getRawData(&r, &g, &b, &c);
-  // Calculate color temperature and lux
   uint16_t colorTemp = tcs1.calculateColorTemperature(r, g, b);
+  //Serial.println("CS-1 Color Temp: "+ String(colorTemp));
   uint16_t lux = tcs1.calculateLux(r, g, b);
-  //Print the color data
-  Serial.print("Red: ");
-  Serial.print(r);
-  Serial.print(" ");
-  Serial.print("Green: ");
-  Serial.print(g);
-  Serial.print(" ");
-  Serial.print("Blue: ");
-  Serial.print(b);
-  Serial.print(" ");
-  Serial.print("Clear: ");
-  Serial.print(c);
-  Serial.print(" ");
-  Serial.print("Color Temperature: ");
-  Serial.print(colorTemp, DEC);
-  Serial.print(" K ");
-  Serial.print("Lux: ");
-  Serial.println(lux, DEC);
-  delay(1000);
-  //Perform the logic here
+  Serial.println("CS-1 : Red: "+ String(r)+", Green: "+String(g)+", Blue: "+String(b) +", Clear: "+String(c) +", Color Temp: "+ String(colorTemp));
 }
 
-void backcoloursensor() {
+void back_colour_sensor() {
+
   TCA9548A(1);
-  //delay(100);
   uint16_t r, g, b, c;
-  // Read the color data from the TCS34725 sensor
-  tcs1.getRawData(&r, &g, &b, &c);
-  // Calculate color temperature and lux
-  uint16_t colorTemp = tcs2.calculateColorTemperature(r, g, b);
+  tcs2.getRawData(&r, &g, &b, &c);
+  uint16_t colorTemp2 = tcs2.calculateColorTemperature(r, g, b);
+  //Serial.println("CS-2 Color Temp: "+ String(colorTemp2));
   uint16_t lux = tcs2.calculateLux(r, g, b);
-  //Print the color data
-  Serial.print("Red: ");
-  Serial.print(r);
-  Serial.print(" ");
-  Serial.print("Green: ");
-  Serial.print(g);
-  Serial.print(" ");
-  Serial.print("Blue: ");
-  Serial.print(b);
-  Serial.print(" ");
-  Serial.print("Clear: ");
-  Serial.print(c);
-  Serial.print(" ");
-  Serial.print("Color Temperature: ");
-  Serial.print(colorTemp, DEC);
-  Serial.print(" K ");
-  Serial.print("Lux: ");
-  Serial.println(lux, DEC);
-  delay(1000);
-  //Perform the logic here as needed
+  Serial.println("CS-2 : Red: "+ String(r)+", Green: "+String(g)+", Blue: "+String(b) +", Clear: "+String(c) +", Color Temp: "+ String(colorTemp2));
+
+}
+
+void loop() {
+  front_colour_sensor();
+  // back_colour_sensor();
 }
